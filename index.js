ab944d63909ed0b329eaf0d24ff1e3e67b061e0b
shenyu <PickStone78@163.com>
2017-03-15 22:18:39 +0800

    Add log for console

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.post('/publish', function (request, response) {
   var exchange = request.body.exchange;
   try {
     var ret = router.publish(exchange);
+    console.log('Publish: ' + exchange);
     response.status(200).send({channel: ret});
   } catch (error) {
     response.status(500).send(error);
@@ -20,6 +21,7 @@ app.post('/subscribe', function (request, response) {
   var exchange = request.body.exchange;
   try {
     var ret = router.subscribe(exchange);
+    console.log('Subscribe: ' + exchange);
     response.status(200).send({channel: ret});
   } catch (error) {
     response.status(500).send(error);
@@ -31,6 +33,7 @@ app.post('/push', function (request, response) {
   var message = request.body.message;
   try {
     var ret = router.push(channel, message);
+    console.log('Push: %s %s', channel, message);
     response.status(200).send({channel: ret});
   } catch (error) {
     response.status(500).send(error);
@@ -41,6 +44,7 @@ app.post('/fetch', function (request, response) {
   var channel = request.body.channel;
   try {
     var ret = router.fetch(channel);
+    console.log('Fetch: %s', channel);
     response.status(200).send({message: ret});
   } catch (error) {
     response.status(500).send(error);
